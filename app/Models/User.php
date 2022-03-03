cb7fd632cad2c63a2fd66daa28b9c38d0a9cfc33
Guilherme França <dankan282@gmail.com>
2022-03-03 20:31:57 -0300

    Using Hash Facade

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Passport\HasApiTokens;
+use Illuminate\Support\Facades\Hash;
 
 class User extends Authenticatable
 {
@@ -44,7 +45,7 @@ class User extends Authenticatable
     ];
 
     public function setPasswordAttribute($password){
-        $this->attributes['password'] = \Hash::make($password);
+        $this->attributes['password'] = Hash::make($password);
     }
 
     public function active_group(){
